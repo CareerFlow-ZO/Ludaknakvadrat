4887d7db5b0137bebde1aa4db4717982f069e4b3
Ludak Na Kvadrat <zaposlitev.rs@gmail.com>
2026-09-14 00:46:31 +0200

    Update stripe-webhook.js

--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -1,7 +1,6 @@
 const Stripe = require("stripe");
 const { Resend } = require("resend");
 const { supabase } = require("../_supabase");
-const { Resend } = require("resend");
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 module.exports=async(req,res)=>{
