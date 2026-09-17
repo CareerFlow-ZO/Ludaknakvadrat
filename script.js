db33cd948bd4ad77c08e526be0463f11c6ed43ca
Ludak Na Kvadrat <zaposlitev.rs@gmail.com>
2026-09-17 23:16:24 +0200

    Load final form translations last

--- script.js
+++ script.js
@@ -112,6 +112,7 @@
     await safeLoad('/lnk-click-fix.js?v=3');
     await safeLoad('/lnk-logo-final.js?v=1');
     await safeLoad('/lnk-price-fix.js?v=1');
+    await safeLoad('/lnk-form-final.js?v=1');
 
     clearTimeout(bootFallback);
     finishBoot();
